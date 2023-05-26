49c51e8ebe521c4b00d15d15dc27942798e48b6f
zhangxh <18511914500@163.com>
2023-05-26 13:29:23 +0800

    2023-5-26

--- musicobj02/src/api/index.js
+++ musicobj02/src/api/index.js
@@ -8,5 +8,10 @@ export function getBanner(type=0){
 }  
 
 
+// 封装获取轮播图的api   调用例子 : /personalized?limit=30   limit=10，50，100
+export function getMusic(limit=10){
+    return axios.get(`${baseUrl}/personalized?limit=${limit}`);
+}  
+
 // 对外抛出
-export default { getBanner };
+export default { getBanner,getMusic };
